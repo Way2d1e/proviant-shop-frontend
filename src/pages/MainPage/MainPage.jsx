c9b305f69362176d6e453ea7fcfa25b580bbb33a
Ytoky <iwadie245@gmail.com>
2022-05-06 14:22:18 +0300

    Added header and footer components.

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MainPage.module.css'
+
+const MainPage = () => {
+    return (
+        <div>
+            hui
+        </div>
+    );
+};
+
+export default MainPage;
