567d0ec9c075ed302e281e14438a7e6c7e8944d3
Alexander Stelmachenko <crazzzygh@gmail.com>
2022-05-06 11:33:26 +0300

    Project setup

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+export const App = () => {
+    return (
+        <h1>
+            <div>
+                123
+            </div>
+        </h1>
+    )
+}
