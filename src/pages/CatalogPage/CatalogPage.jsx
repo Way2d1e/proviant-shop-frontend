8012f2e1ea62bb39b458a531b0fde0b9639b76ac
Kirill <kirill_alexandr0vich@mail.ru>
2022-05-08 16:16:16 +0300

    added keys, changed classes, changed the order of style properties

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,31 +1,29 @@
 import React from 'react'
-import styles from './Catalog.module.css'
+import styles from './CatalogPage.module.css'
 
-const Catalog = () => {
-    const catalogItems = [
-        {id: 0, img: 'src/Assets/Images/vedgetables.png', title: 'Овощи' },
+export const CatalogPage = () => {
+    const catalogCategories = [
+        {id: 0, img: 'src/Assets/Images/vegetables.png', title: 'Овощи' },
         {id: 1, img: 'src/Assets/Images/fruits.png', title: 'Фрукты' },
-        {id: 2, img: 'src/Assets/Images/driedFruits.png', title: 'Сухофрукты'},
+        {id: 2, img: 'src/Assets/Images/driedFruits.png', title: 'Сухофрукты',},
         {id: 3, img: 'src/Assets/Images/spices.png', title: 'Специи' },
-        {id: 4, img: 'src/Assets/Images/bakeryЗroducts.png', title: 'Хлебобулочные изделия',},
+        {id: 4, img: 'src/Assets/Images/bakeryProducts.png', title: 'Хлебобулочные изделия' },
         {id: 5, img: 'src/Assets/Images/nuts.png', title: 'Орехи' },
         {id: 6, img: 'src/Assets/Images/tea.png', title: 'Чай' },
         {id: 7, img: 'src/Assets/Images/souses.png', title: 'Соусы' },
-        {id: 8, img: 'src/Assets/Images/everythingForBaking.png', title: 'Все для выпечки',},
+        {id: 8, img: 'src/Assets/Images/everythingForBaking.png', title: 'Все для выпечки' },
         {id: 9, img: 'src/Assets/Images/sweets.png', title: 'Сладости' },
         {id: 10, img: 'src/Assets/Images/cereals.png', title: 'Крупы' },
         {id: 11, img: 'src/Assets/Images/other.png', title: 'Прочее' },
     ]
 
-    const createCatalogItem = (items) => {
+    const createCatalogCategories = (items) => {
         return (
-            <div className={styles.catalogItemWrap}>
+            <div className={styles.catalogCategoriesWrapper}>
                 {items.map((item) => (
-                    <div className={styles.catalogItem}>
-                        <div>
-                            <img src={item.img} alt="" />
-                        </div>
-                        <div className={styles.titleCatalogItem}>
+                    <div className={styles.catalogCategory} key={item.id}>
+                        <img src={item.img} alt="" />
+                        <div className={styles.catalogCategoryTitle}>
                             <p>{item.title}</p>
                         </div>
                     </div>
@@ -35,11 +33,9 @@ const Catalog = () => {
     }
 
     return (
-        <div className={styles.catalogWrap}>
+        <div className={styles.catalogPage}>
             <div className={styles.titlePage}>Каталог</div>
-            {createCatalogItem(catalogItems)}
+            {createCatalogCategories(catalogCategories)}
         </div>
     )
 }
-
-export default Catalog;
