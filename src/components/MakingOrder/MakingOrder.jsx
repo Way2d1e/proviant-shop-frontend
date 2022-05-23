dad7125c407f545b805b1f7fc9bdacc0b55244bc
Ytoky <iwadie245@gmail.com>
2022-05-23 16:39:58 +0300

    added logic on component making-order

--- src/components/MakingOrder/MakingOrder.jsx
+++ src/components/MakingOrder/MakingOrder.jsx
@@ -1,21 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './MakingOrder.module.css'
 import postOrder from '../../db/post.json'
-import axios from "axios";
+import axios from 'axios'
+import { productsService } from '../../services/productsService'
+import { Loader } from '../UI/Loader'
 
 export const MakingOrder = (props) => {
     const { closeModal } = props
 
-    // const postRequest = (e) => {
-    //     e.preventDefault()
-    //     console.log(postOrder)
-    //     axios.post("http://26.85.60.200:8080/order", postOrder).then(Response =>{
-    //         console.log(Response)
-    //     }).catch(error =>{
-    //         console.log(error)
-    //     })
-    //
-    // }
+    const orderRef = useRef(null)
+
+    const [orderNumber, setOrderNumber] = useState(null)
+
+    const createOrder = (e) => {
+        e.preventDefault()
+        setModalOrder(!modalOrder)
+        const orderItems = productsService.createItemOrders()
+        const formData = new FormData(orderRef.current)
+        let client = {}
+        formData.forEach((item, index) => {
+            client = { ...client, [index]: item }
+        })
+        console.log(client)
+
+        productsService
+            .createOrder({
+                orderItems,
+                client,
+            })
+            .then((res) => {
+                setOrderNumber(res)
+            })
+    }
+
+    const [modalOrder, setModalOrder] = useState(true)
 
     return (
         <div
@@ -28,77 +46,133 @@ export const MakingOrder = (props) => {
                 className={styles.container}
                 onClick={(e) => e.stopPropagation()}
             >
-                <div className={styles.wrapOrder}>
-                    <p>Данные покупателя</p>
-                    <p>Информация о заказе</p>
-                </div>
-                <hr className={styles.line} />
-                <div className={styles.wrapOrder}>
-                    <div className={styles.infoBuyer}>
-                        <input placeholder="Иванов" type="text" />
-                        <input placeholder="Иван" type="text" />
-                        <input placeholder="Иванович" type="text" />
-                        <input placeholder="ivan@mail.ru" type="email" />
-                        <input placeholder="81234567890" type="tel" />
+                <div className={modalOrder ? null : styles.hidden}>
+                    <div
+                        className={
+                            modalOrder ? styles.wrapOrder : styles.hidden
+                        }
+                    >
+                        <p>Данные покупателя</p>
+                        <p>Информация о заказе</p>
                     </div>
-                    <div className={styles.orderInfo}>
-                        <div className={styles.textOrderInfo}>
-                            При нажатии на кнопку
-                            <span
-                                style={{
-                                    color: '#46B14B',
-                                    textDecoration: 'underline',
-                                }}
-                            >
-                                Подтвердить заказ
-                            </span>
-                            на указанную Вами почту прийдет чек.В течении 24
-                            часов нужно будет прийти в магазин с этим чеком,
-                            оплатить и забрать заказ.Через 24 часа заказ
-                            считается недействительным. Оплата производится
-                            любым удобным для Вас способом.
+                    <hr className={styles.line} />
+                    <div className={styles.wrapOrder}>
+                        <div className={styles.infoBuyer}>
+                            <form ref={orderRef} className={styles.formOrder}>
+                                <input
+                                    placeholder="Иванов"
+                                    type="text"
+                                    name="surname"
+                                />
+                                <input
+                                    placeholder="Иван"
+                                    name="name"
+                                    type="text"
+                                />
+                                <input
+                                    placeholder="Иванович"
+                                    name="middleName"
+                                    type="text"
+                                />
+                                <input
+                                    placeholder="ivan@mail.ru"
+                                    name="email"
+                                    type="email"
+                                />
+                                <input
+                                    placeholder="81234567890"
+                                    name="phoneNumber"
+                                    type="phone"
+                                />
+                            </form>
                         </div>
-                        <div className={styles.btnOrder}>
-                            <button
-                                className={styles.btnConfirm}
-                                onClick={(e) => postRequest(e)}
-                            >
-                                Подтвердить заказ
-                            </button>
-                            <button
-                                className={styles.btnCancel}
-                                onClick={() => {
-                                    closeModal(false)
-                                }}
-                            >
-                                Отмена
-                            </button>
+                        <div className={styles.orderInfo}>
+                            <div className={styles.textOrderInfo}>
+                                При нажатии на кнопку
+                                <span
+                                    style={{
+                                        color: '#46B14B',
+                                        textDecoration: 'underline',
+                                    }}
+                                >
+                                    Подтвердить заказ
+                                </span>
+                                на указанную Вами почту прийдет чек.В течении 24
+                                часов нужно будет прийти в магазин с этим чеком,
+                                оплатить и забрать заказ.Через 24 часа заказ
+                                считается недействительным. Оплата производится
+                                любым удобным для Вас способом.
+                            </div>
+                            <div className={styles.btnOrder}>
+                                <button
+                                    className={styles.btnConfirm}
+                                    onClick={(e) => createOrder(e)}
+                                >
+                                    Подтвердить заказ
+                                </button>
+                                <button
+                                    className={styles.btnCancel}
+                                    onClick={() => {
+                                        closeModal(false)
+                                    }}
+                                >
+                                    Отмена
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                {/*<div className={styles.wrapApproveOrder}>*/}
-                {/*    <div>*/}
-                {/*        <div className={styles.approveTitle}>*/}
-                {/*            Спасибо за заказ*/}
-                {/*        </div>*/}
-                {/*        <div className={styles.approveBody}>*/}
-                {/*            <div className={styles.approveOrder}>*/}
-                {/*                Номер заказа*/}
-                {/*            </div>*/}
-                {/*            <div className={styles.approveOrderNumber}>*/}
-                {/*                #123*/}
-                {/*            </div>*/}
-                {/*            <div>*/}
-                {/*                Ожидайте сообщения на почте и предъявите его*/}
-                {/*                продавцу.*/}
-                {/*            </div>*/}
-                {/*            <div>Вскоре с вами свяжется наш оператор</div>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*    <div>*/}
-                {/*        <button className={styles.approveButton}>Ок</button>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                <div
+                    className={
+                        modalOrder ? styles.hidden : styles.blockApproveOrder
+                    }
+                >
+                    {orderNumber ? (
+                        <div
+                            className={
+                                modalOrder
+                                    ? styles.hidden
+                                    : styles.wrapApproveOrder
+                            }
+                        >
+                            <div>
+                                <div className={styles.approveTitle}>
+                                    Спасибо за заказ
+                                </div>
+                                <div className={styles.approveBody}>
+                                    <div className={styles.approveOrder}>
+                                        Номер заказа
+                                    </div>
+                                    <div className={styles.approveOrderNumber}>
+                                        #{orderNumber}
+                                    </div>
+                                    <div className={styles.orderTips}>
+                                        Ожидайте сообщения на почте и предъявите
+                                        его продавцу.
+                                    </div>
+                                    <div className={styles.orderTips}>
+                                        Вскоре с вами свяжется наш оператор
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <button
+                                    onClick={() => {
+                                        closeModal(false)
+                                    }}
+                                    className={styles.approveButton}
+                                >
+                                    Ок
+                                </button>
+                            </div>
+                        </div>
+                    ) : (
+                        <div>
+                            <div className={styles.loaderTip}>Оформляем заказ, ожидайте...</div>
+                            <Loader />
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
--- src/services/productsService.jsx
+++ src/services/productsService.jsx
@@ -1,7 +1,7 @@
 export const productsService = {
     _api: 'http://26.85.60.200:8080/',
 
-    async getResource(url, options)  {
+    async getResource(url, options) {
         const res = await fetch(`${this._api}${url}`, options)
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, received ${res.status}`)
@@ -9,7 +9,7 @@ export const productsService = {
         return res.json()
     },
 
-    async getCategories()  {
+    async getCategories() {
         return await this.getResource('category')
     },
 
@@ -20,19 +20,32 @@ export const productsService = {
     async addProductToCard(product) {
         if (localStorage.getItem('products')) {
             const products = JSON.parse(localStorage.getItem('products'))
-            localStorage.setItem('products', JSON.stringify([...products, product]))
+            localStorage.setItem(
+                'products',
+                JSON.stringify([...products, product])
+            )
         } else {
             localStorage.setItem('products', JSON.stringify([product]))
         }
     },
 
     async setCartProduct() {
-        const products = await fetch('http://localhost:3001/products').then((res) => res.json())
+        const products = await fetch('http://localhost:3001/products').then(
+            (res) => res.json()
+        )
         await window.localStorage.setItem('products', JSON.stringify(products))
     },
 
     async getCartProducts() {
         return JSON.parse(window.localStorage.getItem('products'))
+    },
+
+    createItemOrders() {
+        return JSON.parse(localStorage.getItem('products')).map(
+            ({ id: productId, weight }) => {
+                return { productId, weight }
+            }
+        )
     },
 
     async createOrder(order) {
